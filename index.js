7b7b2476ac4267015b52d56d58654d4190305e80
Whitny Edwards <wne4e2@gmail.com>
2019-06-15 17:15:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(number){
+  let message
+   if(number <= 400) {
+     message = "This one is on me!"
+   } else if(number > 2000 && number < 2500) {
+     message = "I will gladly take your thirty bucks."
+   } else if(number > 2500) {
+     message = "No can do."
+   }
+   return message
+ }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let result = city === "NYC" ? "Ok, sounds good." : "No go."
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+   return result
+ }
+
+function switchOnCharmFromTip(tip){
+  let response;
+    switch (tip) {
+      case "generous" :
+      response = "Thank you so much.";
+      break;
+      case "not as generous" :
+      response = "Thank you.";
+      break;
+      case "thanks for everything" :
+      response = "Bye.";
+      break;
+    }
+    return response;
+  }
